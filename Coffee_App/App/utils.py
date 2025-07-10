a7ea016412183296048c3ba9e1cde5d34cadd3c5
Anupamsingh1812 <anupamsingh20021812@gmail.com>
2025-07-10 09:33:42 +0530

    Delete Coffee_App directory

--- Coffee_App/App/utils.py
+++
@@ -1,9 +0,0 @@
-def is_valid_quantity(quantity_str):
-    try:
-        quantity = int(quantity_str)
-        return quantity > 0
-    except ValueError:
-        return False
-
-def calculate_price(unit_price, quantity):
-    return unit_price * quantity
--- Coffee_App/main.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from App.coffee_app import CoffeeApp
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = CoffeeApp(root)
-    root.mainloop()
-
-
